dbd4df636746b0662d0548a0d60e53c0a0714ae3
Woifgang <wolfgang.aschenbrenner@mail.de>
2018-03-04 20:06:44 +0100

    routing

--- blog/public/index.php
+++ blog/public/index.php
@@ -4,12 +4,24 @@ require __DIR__ . "/../init.php";
 $pathInfo = $_SERVER['PATH_INFO'];
 var_dump($pathInfo);
 
-if ($pathInfo == "/index") {
-    $postsController = $container->make("postsController");
-    $postsController->index();
-} elseif ($pathInfo == "/post") {
-    $postController = $container->make("postsController");
-    $postController->post();
+$routes = [
+    '/index' => [
+        'controller' => 'postsController',
+        'method' => 'index'
+    ],
+    '/post' => [
+        'controller' => 'postsController',
+        'method' => 'post'
+    ]
+];
+
+if (isset($routes[$pathInfo]))
+{
+    $route = $routes[$pathInfo];
+    $controller = $container->make($route['controller']);
+    $method = $route['method'];
+    $controller->$method();
+
 }
 
 ?>
