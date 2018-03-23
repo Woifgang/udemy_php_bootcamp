b928ae6bfed793b9a12d79028c4ef42f3350905a
Woifgang <wolfgang.aschenbrenner@mail.de>
2018-03-23 08:20:37 +0100

    Update Post Button verarbeiten

---
+++ blog/src/Post/PostsAdminController.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Woifgang_Laptop
+ * Date: 22.03.2018
+ * Time: 20:16
+ */
+
+namespace App\Login;
+
+
+use App\Core\AbstractController;
+use App\Post\PostsRepository;
+
+
+class PostsAdminController extends AbstractController
+{
+
+
+    public function __construct(LoginService $loginService, PostsRepository $postsRepository)
+    {
+        $this->loginService = $loginService;
+        $this->postsRepository = $postsRepository;
+    }
+
+    public function showPostEdit()
+    {
+        //Passwortschutz
+        $this->loginService->check();
+
+        //Datenbankabfrage
+        $editPosts = $this->postsRepository->all();
+
+        //Liste Ausgeben
+        $this->render("dashboard/posts-admin", [
+            'editPosts' => $editPosts
+        ]);
+    }
+
+    public function editPost()
+    {
+        $this->loginService->check();
+
+        $id = $_GET['id'];
+
+        $editPost = $this->postsRepository->find($id);
+
+        $this->render("dashboard/edit-post", [
+            'editPost' => $editPost
+        ]);
+    }
+	
+	public function updatePost()
+	{
+		$this->loginService->check();
+		
+		$id = $_GET['id'];
+		
+		if (isset($_POST['title']) OR isset($_POST['content']) )
+		{
+			$title = $_POST['title'];
+			$content = $_POST['content'];
+			
+			$this->postsRepository->updatePost($id, $title, $content);
+		}
+		
+		$editPost = $this->postsRepository-find($id);
+		
+		$this-render("dashboard/edit-post", [
+			'editPost' => $editPost
+		])
+		
+	}
+}
